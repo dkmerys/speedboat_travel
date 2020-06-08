5c059843f80ceea335655287c807a9fccf615009
Daniel Merys <dkmerys@gmail.com>
2020-06-08 13:58:00 -0700

    pass delete route test

--- spec/requests/delete_review_spec.rb
+++ spec/requests/delete_review_spec.rb
@@ -1,3 +1,17 @@
 require 'rails_helper'
 
-describe "delete "
+describe "delete a review route", :type => :request do
+  
+  before do 
+    @review = Review.create( {:id => 20, :author => "Morty", :content => "blah blah blah blah blah blah blah blah blah blah blah blah", :city => "Peru", :country => "Mexico"})
+    delete '/reviews/20'
+  end
+
+  it 'returns the deleted content' do
+    expect(JSON.parse(response.body)['message']).to eq('This review has been successfully deleted!')
+  end
+
+  it 'returns a deleted status' do 
+    expect(response).to have_http_status(200)
+  end
+end
