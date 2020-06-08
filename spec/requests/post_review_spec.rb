370456e4b50cac66397c5b92dc6f1878c303726e
Daniel Merys <dkmerys@gmail.com>
2020-06-08 11:46:14 -0700

    pass post review test and exception test

--- spec/requests/post_review_spec.rb
+++ spec/requests/post_review_spec.rb
@@ -1,28 +1,39 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "post a review route", :type => :request do
+describe "post a review route", :type => :request do
   
-#   before do
-#     post '/reviews', params: {:author => 'test_author', :content => 'test_content', :country => 'Mexico', :city => "Seattle" }
-#   end
+  before do
+    post '/reviews', params: {:author => 'test_author', :content => 'Cleanse hoodie offal asymmetrical franzen celiac beard church-key stumptown fingerstache mustache distillery.', :country => 'Mexico', :city => "Seattle" }
+  end
 
-#   it 'returns the author name' do
-#     expect(JSON.parse(response.body)['author']).to eq('test_author')
-#   end
+  it 'returns the author name' do
+    expect(JSON.parse(response.body)['author']).to eq('test_author')
+  end
 
-#   it 'returns the review content' do 
-#     expect(JSON.parse(response.body)['content']).to eq('test_content')
-#   end 
+  it 'returns the review content' do 
+    expect(JSON.parse(response.body)['content']).to eq('Cleanse hoodie offal asymmetrical franzen celiac beard church-key stumptown fingerstache mustache distillery.')
+  end 
 
-#   it 'returns the country name' do 
-#     expect(JSON.parse(response.body)['country']).to eq('Mexico')
-#   end 
+  it 'returns the country name' do 
+    expect(JSON.parse(response.body)['country']).to eq('Mexico')
+  end 
 
-#   it 'returns the city name' do 
-#     expect(JSON.parse(response.body)['city']).to eq('Seattle')
-#   end
+  it 'returns the city name' do 
+    expect(JSON.parse(response.body)['city']).to eq('Seattle')
+  end
 
-#   it 'returns a created status' do
-#     expect(response).to have_http_status(:created)
-#   end
-# end
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
+end
+
+describe "Receive exception on Post review route", :type => :request do
+
+  before do
+    post '/reviews', params: {:author => '', :content => 'Cleanse hoodie offal asymmetrical franzen celiac beard church-key stumptown fingerstache mustache distillery.', :country => 'Mexico', :city => "Seattle" }
+  end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+end
