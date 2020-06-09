b0d130edd7bcadb4846273ca616ae00ffcebc882
Daniel Merys <dkmerys@gmail.com>
2020-06-09 09:05:16 -0700

    add kaminari gem for pagination

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jquery-rails'
 gem 'httparty'
 gem 'jwt'
 gem 'simple_command'
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::API
   end
 
   private
-def authenticate_request
-  @current_user = AuthorizeApiRequest.call(request.headers).result
-  render json: { error: 'Not Authorized' }, status: 401 unless @current_user
- end 
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end 
 end
